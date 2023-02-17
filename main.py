fd082f0013b3393f7e5d3bee1d947f63b34c4527
lastis-citra <lastis@infoseek.jp>
2023-02-18 04:02:32 +0900

    ファイル名が変更なしの場合にも連番処理が実行されてしまうバグ修正，jav380を追加

--- main.py
+++ main.py
@@ -4,6 +4,33 @@ from bs4 import BeautifulSoup
 import cloudscraper
 
 
+# https://www.jav380.com/ から情報を取り出す
+def get_jav380_data(fc2_id):
+    url = f'https://www.jav380.com/?s=FC2+PPV+{fc2_id}'
+    # print(f'fc2cm_url: {url} ', end='')
+    scraper = cloudscraper.create_scraper(
+        browser={
+            'browser': 'chrome',
+            'platform': 'windows',
+            'desktop': True
+        }
+    )
+    res = scraper.get(url)
+    # res.encoding = res.apparent_encoding
+    soup = BeautifulSoup(res.content, 'html.parser')
+    a_tags = soup.select('a[class="more-link"]')
+    if len(a_tags) > 0:
+        content_url = a_tags[-1]["href"]
+        res = scraper.get(content_url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        contents = soup.select_one('div[class="post-content"]')
+        for content in contents.text.splitlines():
+            if '販売者' in content:
+                # print(content)
+                return content.replace('販売者 ', '')
+    return ''
+
+
 # https://fc2cm.com/ から情報を取り出す
 def get_fc2cm_data(fc2_id):
     url = f'https://fc2cm.com/?p={fc2_id}'
@@ -93,7 +120,7 @@ def rename_dir(path):
     file_names = sorted(file_names)
 
     count = len(file_names)
-    i = 0
+    i = 1
     for file_name in file_names:
         print(f'Renaming... {i}/{count} {file_name}')
         # すでに変換済みのファイルは除く，undefinedが付いているファイルはもう1回検索する
@@ -114,6 +141,12 @@ def rename_dir(path):
                     seller = get_fc2cm_data(fc2_id)
                 if seller == '':
                     user = UNDEFINED_NAME
+                # fc2cmで見つからない場合はjav380で探す
+                if seller == '':
+                    print('Searching in jav380...')
+                    seller = get_jav380_data(fc2_id)
+                if seller == '':
+                    user = UNDEFINED_NAME
                 else:
                     user = f'(By {seller})'
                 # undefinedが付いているファイルはその部分を置換する
@@ -128,10 +161,15 @@ def rename_dir(path):
                     base_name = [base_name[x:x + MAX_LENGTH] for x in range(0, len(base_name), MAX_LENGTH)][0]
                 # 禁則処理
                 base_name = re.sub(r'[\\/:*?"<>|]+', '', base_name)
-                # 同名ファイルがある場合は連番にする
-                new_file_name = duplicate_rename(os.path.join(path, base_name + ext))
-                print(f'Rename to: {new_file_name}')
-                os.rename(os.path.join(path, file_name), os.path.join(path, new_file_name))
+                # 同名ファイルがある場合かつ自分自身でない場合は連番にする
+                new_file_path = os.path.join(path, base_name + ext)
+                if os.path.join(path, file_name) != new_file_path:
+                    new_file_path = duplicate_rename(os.path.join(path, base_name + ext))
+                print(f'Rename to: {new_file_path}')
+                try:
+                    os.rename(os.path.join(path, file_name), os.path.join(new_file_path))
+                except OSError:
+                    print(f'OSError')
         else:
             print('Already renamed!!!')
         i += 1
